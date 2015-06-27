27c982f0639ced8b433f38fcfb518ea56e68a6b9
André Tietz <andre.tietz@gmail.com>
2015-06-27 13:57:10 +0200

    removed unused exceptions and made the composite interceptor more convinient

--- retroauth/src/main/java/eu/unicate/retroauth/NoAccountAvailableException.java
+++
@@ -1,7 +0,0 @@
-package eu.unicate.retroauth;
-
-/**
- * Created by André on 15.06.2015.
- */
-public class NoAccountAvailableException extends RuntimeException {
-}
--- retroauth/src/main/java/eu/unicate/retroauth/interceptors/CompositeRequestInterceptor.java
+++ retroauth/src/main/java/eu/unicate/retroauth/interceptors/CompositeRequestInterceptor.java
@@ -1,5 +1,8 @@
-package eu.unicate.retroauth;
+package eu.unicate.retroauth.interceptors;
 
+import android.support.annotation.NonNull;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit.RequestInterceptor;
@@ -9,8 +12,12 @@ public class CompositeRequestInterceptor implements RequestInterceptor {
 	private final List<RequestInterceptor> interceptors;
 
 
-	public CompositeRequestInterceptor(List<RequestInterceptor> interceptors) {
-		this.interceptors = interceptors;
+	public CompositeRequestInterceptor() {
+		this.interceptors = new ArrayList<>();
+	}
+
+	public void addRequestIntercetor(@NonNull RequestInterceptor interceptor) {
+		interceptors.add(interceptor);
 	}
 
 	@Override
