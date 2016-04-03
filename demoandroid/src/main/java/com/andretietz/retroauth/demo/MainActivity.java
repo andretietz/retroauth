17981065a72d2ea62ce6596b0a3fd7110849dcd7
André Tietz <andre.tietz@gmail.com>
2016-04-03 20:12:44 +0200

    removed databindung

--- demoandroid/src/main/java/com/andretietz/retroauth/demo/MainActivity.java
+++ demoandroid/src/main/java/com/andretietz/retroauth/demo/MainActivity.java
@@ -2,7 +2,6 @@ package com.andretietz.retroauth.demo;
 
 import android.accounts.AccountManager;
 import android.content.Context;
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -15,7 +14,6 @@ import com.andretietz.retroauth.AuthAccountManager;
 import com.andretietz.retroauth.LogInterceptor;
 import com.andretietz.retroauth.Retroauth;
 import com.andretietz.retroauth.demo.GithubService.Email;
-import com.andretietz.retroauth.demo.databinding.ActivityMainBinding;
 
 import java.util.List;
 
@@ -28,113 +26,121 @@ import retrofit2.converter.moshi.MoshiConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    private GithubService githubService;
-    private AuthAccountManager authAccountManager;
+	private GithubService githubService;
+	private AuthAccountManager authAccountManager;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+	@SuppressWarnings("ConstantConditions")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        /**
-         * Optional: create your own OkHttpClient
-         */
-        OkHttpClient httpClient = new OkHttpClient.Builder()
-              .addInterceptor(new LogInterceptor())
-              .build();
-
-        /**
-         * Create an instance of the {@link AndroidAuthenticationHandler}
-         */
-        Context context = this; // this line is to show that this is a context only
-
-        AndroidAuthenticationHandler authenticationHandler =
-              new AndroidAuthenticationHandler(context, GithubService.INJECTOR);
+		View buttonRequestEmail = findViewById(R.id.buttonRequestEmail);
+		View buttonInvalidateToken = findViewById(R.id.buttonInvalidateToken);
+		View buttonResetPrefAccount = findViewById(R.id.buttonResetPrefAccount);
+		View buttonAddAccount = findViewById(R.id.buttonAddAccount);
 
 
-        /**
-         * Create your Retrofit Object using the {@link Retroauth.Builder}
-         */
-        Retrofit retrofit = new Retroauth.Builder<>(authenticationHandler)
-              .methodCache(new AndroidMethodCache()) // optional: using sparsearray instead of hashmap
-              .baseUrl("https://api.github.com")
-              .client(httpClient)
-              .addConverterFactory(MoshiConverterFactory.create())
-              .build();
+		/**
+		 * Optional: create your own OkHttpClient
+		 */
+		OkHttpClient httpClient = new OkHttpClient.Builder()
+				.addInterceptor(new LogInterceptor())
+				.build();
 
-        /**
-         * Create your API Service
-         */
-        githubService = retrofit.create(GithubService.class);
+		/**
+		 * Create an instance of the {@link AndroidAuthenticationHandler}
+		 */
+		Context context = this; // this line is to show that this is a context only
+
+		AndroidAuthenticationHandler authenticationHandler =
+				new AndroidAuthenticationHandler(context, GithubService.INJECTOR);
 
 
+		/**
+		 * Create your Retrofit Object using the {@link Retroauth.Builder}
+		 */
+		Retrofit retrofit = new Retroauth.Builder<>(authenticationHandler)
+				.methodCache(new AndroidMethodCache()) // optional: using sparsearray instead of hashmap
+				.baseUrl("https://api.github.com")
+				.client(httpClient)
+				.addConverterFactory(MoshiConverterFactory.create())
+				.build();
 
-        binding.buttonRequestEmail.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /**
-                 * Use it!
-                 */
-                githubService.getEmails().enqueue(new Callback<List<Email>>() {
-                    @Override
-                    public void onResponse(Call<List<Email>> call, Response<List<Email>> response) {
-                        if (response.isSuccessful()) { showEmails(response.body()); } else {
-                            show("Error: " + response.message());
-                        }
-                    }
+		/**
+		 * Create your API Service
+		 */
+		githubService = retrofit.create(GithubService.class);
 
-                    @Override
-                    public void onFailure(Call<List<Email>> call, Throwable t) {
-                        showError(t);
-                    }
-                });
-            }
-        });
 
-        authAccountManager = new AuthAccountManager(this);
-        binding.buttonInvalidateToken.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // override the current token to force a 401
-                AccountManager.get(MainActivity.this)
-                      .setAuthToken(
-                            authAccountManager.getActiveAccount(GithubService.ACCOUNT_TYPE, false),
-                            GithubService.TOKEN_TYPE,
-                            "some-invalid-token"
-                      );
-            }
-        });
+		buttonRequestEmail.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				/**
+				 * Use it!
+				 */
+				githubService.getEmails().enqueue(new Callback<List<Email>>() {
+					@Override
+					public void onResponse(Call<List<Email>> call, Response<List<Email>> response) {
+						if (response.isSuccessful()) {
+							showEmails(response.body());
+						} else {
+							show("Error: " + response.message());
+						}
+					}
 
-        binding.buttonResetPrefAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                authAccountManager.resetActiveAccount(GithubService.ACCOUNT_TYPE);
-            }
-        });
+					@Override
+					public void onFailure(Call<List<Email>> call, Throwable t) {
+						showError(t);
+					}
+				});
+			}
+		});
 
-        binding.buttonAddAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                authAccountManager.addAccount(MainActivity.this, GithubService.ACCOUNT_TYPE, GithubService.TOKEN_TYPE);
-            }
-        });
-    }
+		authAccountManager = new AuthAccountManager(this);
+		buttonInvalidateToken.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// override the current token to force a 401
+				AccountManager.get(MainActivity.this)
+						.setAuthToken(
+								authAccountManager.getActiveAccount(GithubService.ACCOUNT_TYPE, false),
+								GithubService.TOKEN_TYPE,
+								"some-invalid-token"
+						);
+			}
+		});
 
-    private void showEmails(List<Email> emailList) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Your protected emails:\n");
-        for (int i = 0; i < emailList.size(); i++) {
-            sb.append(emailList.get(i)).append('\n');
-        }
-        show(sb.toString());
-    }
+		buttonResetPrefAccount.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				authAccountManager.resetActiveAccount(GithubService.ACCOUNT_TYPE);
+			}
+		});
 
-    private void show(String toShow) {
-        Toast.makeText(this, toShow, Toast.LENGTH_SHORT).show();
-    }
+		buttonAddAccount.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				authAccountManager.addAccount(MainActivity.this, GithubService.ACCOUNT_TYPE, GithubService.TOKEN_TYPE);
+			}
+		});
+	}
 
-    private void showError(Throwable error) {
-        error.printStackTrace();
-        show(error.toString());
-    }
+	private void showEmails(List<Email> emailList) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Your protected emails:\n");
+		for (int i = 0; i < emailList.size(); i++) {
+			sb.append(emailList.get(i).email).append('\n');
+		}
+		show(sb.toString());
+	}
+
+	private void show(String toShow) {
+		Toast.makeText(this, toShow, Toast.LENGTH_SHORT).show();
+	}
+
+	private void showError(Throwable error) {
+		error.printStackTrace();
+		show(error.toString());
+	}
 }
