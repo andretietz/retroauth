951d848ba37afb101c666529a6f30a8f7695f824
André Tietz <andre.tietz@gmail.com>
2016-06-20 20:53:22 +0200

    reintroduced google client

--- demoandroid/src/main/java/com/andretietz/retroauth/demo/ProviderGoogle.java
+++ demoandroid/src/main/java/com/andretietz/retroauth/demo/ProviderGoogle.java
@@ -18,9 +18,9 @@ import retrofit2.Retrofit;
  */
 public class ProviderGoogle implements Provider<Account, AndroidTokenType, AndroidToken> {
 
-    public static final String GOOGLE_CLIENT_ID = "<your-client-id>";
-    public static final String GOOGLE_CLIENT_SECRET = "<your-client-secret>";
-    public static final String GOOGLE_CLIENT_CALLBACK = "<your-client-callback>";
+    public static final String GOOGLE_CLIENT_ID = "329078189044-q3g29v14uhnrbb5vsaj8d34j26vh4fb4.apps.googleusercontent.com";
+    public static final String GOOGLE_CLIENT_SECRET = "HOePqkgIemKIcNhfRt8_jpfF";
+    public static final String GOOGLE_CLIENT_CALLBACK = "http://localhost:8000/accounts/google/login/callback/";
 
     private GoogleService googleService;
 
