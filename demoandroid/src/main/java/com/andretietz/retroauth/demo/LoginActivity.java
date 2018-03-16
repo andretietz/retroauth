06ea4c78256d7e6406fa7d5088c6e7c4a3dd9f18
André Tietz <andre.tietz@gmail.com>
2018-03-16 15:22:15 +0100

    removed pointless User agent from demo

--- demoandroid/src/main/java/com/andretietz/retroauth/demo/LoginActivity.java
+++ demoandroid/src/main/java/com/andretietz/retroauth/demo/LoginActivity.java
@@ -106,19 +106,9 @@ public class LoginActivity extends AuthenticationActivity {
         static OkHttpClient client() {
             HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
             interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-            OkHttpClient client = new OkHttpClient.Builder()
-                    .addInterceptor(new Interceptor() {
-                        @Override
-                        public Response intercept(Chain chain) throws IOException {
-                            Request request = chain.request().newBuilder()
-                                    .header("User-Agent", "Foo")
-                                    .build();
-                            return chain.proceed(request);
-                        }
-                    })
+            return new OkHttpClient.Builder()
                     .addInterceptor(interceptor)
                     .build();
-            return client;
         }
 
         TokenVerifier(OAuth20Service service, String code) {
