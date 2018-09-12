522e83747edddd3ab09626ad740709e84f819c0a
XinyueZ <dev.xinyue.zhao@gmail.com>
2018-09-12 22:13:52 +0200

    Format
    AccountAuthenticator.kt

--- retroauth-android/src/main/java/com/andretietz/retroauth/AccountAuthenticator.kt
+++ retroauth-android/src/main/java/com/andretietz/retroauth/AccountAuthenticator.kt
@@ -26,25 +26,17 @@ import android.os.Bundle
 
 /**
  * This AccountAuthenticator is a very basic implementation of Android's
- * [AbstractAccountAuthenticator]. This implementation is intentional as empty as it is. Cause of this is, that
- * it's executed in a different process, which makes it difficult to provide login endpoints from the app process in here.
+ * [AbstractAccountAuthenticator]. This implementation is intentional as empty as it is.
+ * Cause of this is, that it's executed in a different process,
+ * which makes it difficult to provide login endpoints from the app process in here.
  *
  *
- * NOTE: This class cannot be replaced with a kotlin version yet, since Android cannot load Authenticators
- * that are non java once
+ * NOTE: This class cannot be replaced with a kotlin version yet, since Android cannot load
+ * Authenticators that are non java once.
  */
-class AccountAuthenticator
-/**
- * @param context The context (needed by the [AbstractAccountAuthenticator]
- * @param action  The Action String to open the Activity to login
- */
-    (
-    context: Context,
-    /**
-     * The Action string to open the implementation of the [AuthenticationActivity],
-     * to show the user a login.
-     */
-     val action: String
+class AccountAuthenticator(
+  context: Context,
+  val action: String
 ) : AbstractAccountAuthenticator(context) {
 
   override fun addAccount(
@@ -55,59 +47,58 @@ class AccountAuthenticator
     options: Bundle?
   ) = createAuthBundle(response, accountType, authTokenType, null)
 
-    override fun getAuthToken(
-        response: AccountAuthenticatorResponse, account: Account,
-        authTokenType: String, options: Bundle
-    ) = createAuthBundle(response, account.type, authTokenType, account.name)
+  override fun getAuthToken(
+    response: AccountAuthenticatorResponse,
+    account: Account,
+    authTokenType: String,
+    options: Bundle
+  ) = createAuthBundle(response, account.type, authTokenType, account.name)
 
-    /**
-     * Creates an Intent to open the Activity to login.
-     *
-     * @param response    needed parameter
-     * @param accountType The account Type
-     * @param tokenType   The requested token type
-     * @param accountName The name of the account
-     * @return a bundle to open the activity
-     */
-    private fun createAuthBundle(
-      response: AccountAuthenticatorResponse?, accountType: String?,
-      tokenType: String?, accountName: String?
-    ): Bundle {
-        val intent = Intent(action)
-        intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response)
-        intent.putExtra(AccountManager.KEY_ACCOUNT_TYPE, accountType)
-        intent.putExtra(KEY_TOKEN_TYPE, tokenType)
-        if (null != accountName) {
-            intent.putExtra(AccountManager.KEY_ACCOUNT_NAME, accountName)
-        }
-        val bundle = Bundle()
-        bundle.putParcelable(AccountManager.KEY_INTENT, intent)
-        return bundle
+  private fun createAuthBundle(
+    response: AccountAuthenticatorResponse?,
+    accountType: String?,
+    tokenType: String?,
+    accountName: String?
+  ): Bundle {
+    val intent = Intent(action)
+    intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response)
+    intent.putExtra(AccountManager.KEY_ACCOUNT_TYPE, accountType)
+    intent.putExtra(KEY_TOKEN_TYPE, tokenType)
+    if (null != accountName) {
+      intent.putExtra(AccountManager.KEY_ACCOUNT_NAME, accountName)
     }
+    val bundle = Bundle()
+    bundle.putParcelable(AccountManager.KEY_INTENT, intent)
+    return bundle
+  }
 
-    override fun confirmCredentials(
-        response: AccountAuthenticatorResponse,
-        account: Account, options: Bundle
-    ) = null
+  override fun confirmCredentials(
+    response: AccountAuthenticatorResponse,
+    account: Account,
+    options: Bundle
+  ) = null
 
-    override fun editProperties(
-        response: AccountAuthenticatorResponse,
-        accountType: String
-    ) = null
+  override fun editProperties(
+    response: AccountAuthenticatorResponse,
+    accountType: String
+  ) = null
 
-    override fun getAuthTokenLabel(authTokenType: String) = null
+  override fun getAuthTokenLabel(authTokenType: String) = null
 
-    override fun updateCredentials(
-        response: AccountAuthenticatorResponse, account: Account,
-        authTokenType: String, options: Bundle
-    ) = null
+  override fun updateCredentials(
+    response: AccountAuthenticatorResponse,
+    account: Account,
+    authTokenType: String,
+    options: Bundle
+  ) = null
 
-    override fun hasFeatures(
-        response: AccountAuthenticatorResponse, account: Account,
-        features: Array<String>
-    ) = null
+  override fun hasFeatures(
+    response: AccountAuthenticatorResponse,
+    account: Account,
+    features: Array<String>
+  ) = null
 
-    companion object {
-        const val KEY_TOKEN_TYPE = "account_token_type"
-    }
+  companion object {
+    const val KEY_TOKEN_TYPE = "account_token_type"
+  }
 }
