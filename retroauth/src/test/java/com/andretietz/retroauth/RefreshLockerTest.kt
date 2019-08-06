ac99547a899dbbf7668a4e282fd688cc8a2887d3
André Tietz <andre.tietz@gmail.com>
2019-08-06 21:43:50 +0200

    wrote a simplified test for multithreaded calls

--- retroauth/src/test/java/com/andretietz/retroauth/RefreshLockerTest.kt
+++ retroauth/src/test/java/com/andretietz/retroauth/RefreshLockerTest.kt
@@ -1,7 +1,7 @@
 package com.andretietz.retroauth
 
-import com.nhaarman.mockito_kotlin.doAnswer
-import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.times
+import com.nhaarman.mockito_kotlin.verify
 import io.reactivex.Single
 import io.reactivex.observers.TestObserver
 import io.reactivex.schedulers.Schedulers
@@ -14,51 +14,87 @@ import java.util.concurrent.locks.ReentrantLock
 class RefreshLockerTest {
   @Test
   fun testMulti() {
-    val runnable = mock<Runnable> {
-      on { run() } doAnswer {
-        println("Entering ${Thread.currentThread().id} - ${System.currentTimeMillis()} ")
-        Thread.sleep(2000)
-        println("Leaving ${Thread.currentThread().id} - ${System.currentTimeMillis()} ")
-        throw IllegalStateException()
-      }
-    }
-    val amount = 200
-    val list = mutableListOf<TestObserver<String>>()
-    for (i in 0..amount) {
-      list.add(createSingle(runnable).subscribeOn(Schedulers.io()).test())
+    /**
+     * This runnable is simulating a token refresh & get token.
+     * While this, something goes wrong and the refresh throws an exception.
+     */
+    val runnable = Runnable {
+      // refresh...
+      Thread.sleep(200) // make sure it takes longer than 200 request to also try
+      // something goes wrong
+      throw IllegalStateException()
     }
 
-    for (i in 0..amount) {
-      list[i]
-        .awaitTerminalEvent()
-//        .assertError { error -> error is IllegalStateException }
+    // parallel outgoing requests
+    val requestCount = 200
+    // token types
+    val tokenTypes = 1
+    // list of observers to test
+    val list = mutableListOf<TestObserver<String>>()
+    // this is a fake request interceptor
+    val interceptor = FakeInterceptor(runnable)
+
+    for (i in 0..requestCount) {
+      list.add(
+        Single.create<String> { emitter ->
+          emitter.onSuccess(interceptor
+            .doRequest(getLockingObject("lock-${i % tokenTypes}"))
+          )
+        }
+          // push to new thread (io scheduler creates more threads if required)
+          .subscribeOn(Schedulers.io())
+          .test() // subscribe
+      )
     }
+
+    list.map {
+      // waiting until the observable (single) completes
+      it.awaitTerminalEvent()
+      // make sure it was throwing an IllegalStateException
+      it.assertError { error -> error is IllegalStateException }
+    }
+
+    // each token type should be only refreshing the token once.
+    verify(runnable, times(tokenTypes)).run()
   }
 
-  private fun createSingle(runnable: Runnable): Single<String> {
-    return Single.create<String> { emitter ->
-      emitter.onSuccess(TestLocker(runnable).method("asd"))
+  private val lockingObjects = HashMap<String, String>()
+
+  private fun getLockingObject(key: String): String {
+    return if (lockingObjects.containsKey(key)) {
+      lockingObjects[key]!!
+    } else {
+      lockingObjects[key] = key
+      lockingObjects[key]!!
     }
   }
 }
 
-internal class TestLocker(private val doSomething: Runnable) {
+/**
+ * This class is a fake, where in real life this would be our RequestInterceptor
+ */
+internal class FakeInterceptor(private val refreshEmulator: Runnable) {
 
   companion object {
     private val TOKEN_TYPE_LOCKERS = HashMap<Any, AccountTokenLock>()
   }
 
-  fun method(lock: String): String {
+  fun doRequest(lock: String): String {
     try {
       lock(lock)
       try {
-        doSomething.run()
+        refreshEmulator.run()
       } catch (error: Exception) {
         storeAndThrowError(lock, error)
       }
     } finally {
       unlock(lock)
     }
+    // here is where the request would happen
+    // do request...
+
+    // it actually doesn't need to return anything in the test. this is just to have the api
+    // as close to the RL one as possible
     return lock
   }
 
@@ -93,31 +129,34 @@ internal class TestLocker(private val doSomething: Runnable) {
   private fun lock(type: String) {
     val lock = getLock(type)
     if (!lock.lock.tryLock()) {
-      println("waiting nr: ${lock.waitCounter.incrementAndGet()}")
-
+      println("Thread ${Thread.currentThread().id} is waiting.")
+      lock.waitCounter.incrementAndGet()
       lock.lock.lock()
-      println("reenter")
+      println("Thread ${Thread.currentThread().id} is locked.")
       val exception = lock.errorContainer.get()
       if (exception != null) {
         println(exception.toString())
         throw exception
       }
-      println("no exception, go on")
+    } else {
+      println("Thread ${Thread.currentThread().id} is locked.")
     }
   }
 
   private fun unlock(type: String) {
     val lock = getLock(type)
-    println("unlocking: ${lock.waitCounter.get()}")
-    if (lock.waitCounter.decrementAndGet() < 0) {
+    println("unlocking Thread ${Thread.currentThread().id} waiting threads left: ${lock.waitCounter.get()}")
+    if (lock.waitCounter.getAndDecrement() <= 0) {
       lock.errorContainer.set(null)
     }
     lock.lock.unlock()
   }
 
   internal data class AccountTokenLock(
-    val lock: Lock = ReentrantLock(true),
+    val lock: Lock = ReentrantLock(false),
+    // atomic reference required?
     val errorContainer: AtomicReference<Throwable> = AtomicReference(),
+    // atomic integer required?
     val waitCounter: AtomicInteger = AtomicInteger()
   )
 }
